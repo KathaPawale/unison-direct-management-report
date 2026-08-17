3bb9418bedec6ed8bc107523aadbbe908b68f3c2
Katha Pawale <kathapawale321@gmail.com>
2026-08-17 15:07:44 +0530

    Add application configuration

---
+++ js/config.js
@@ -0,0 +1,14 @@
+window.UNISON_CONFIG = Object.freeze({
+  companyName: 'Unison Direct GCC INC',
+  brandName: 'Unison Direct',
+  watermark: 'UNISON DIRECT',
+  disclaimer: 'The report we are submitting is for management purpose only. The numbers are based on data submitted and instructed by client',
+  currency: 'USD',
+  template: {
+    primary: '#1F497D',
+    footer: '#DAE5F1',
+    pageWidth: 612,
+    pageHeight: 792,
+    font: 'Helvetica'
+  }
+});
